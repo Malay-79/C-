8ce95fd5bee5d87e6ab588053b51a6d5ccf62a5a
Malay-79 <malaypratap0709@gmail.com>
2026-08-12 00:29:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.63%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
+    double power(double x , long long n){
+        if(n==0) return 1.0;
+        if(n==1) return x;
+
+        if(n%2==0) return power(x*x,n/2);
+        return x*power(x,n-1);
+    }
     double myPow(double x, int n) {
-        double ans = 1.0;
-        long long nn = n;
-        if(nn<0) nn = -1*nn;
-        while(nn){
-            if(nn%2==1){
-                ans = ans*x;
-                nn = nn-1;
-            }else{
-                x = x*x;
-                nn=nn/2;
-            }
+        // double ans = 1.0;
+        // long long nn = n;
+        // if(nn<0) nn = -1*nn;
+        // while(nn){
+        //     if(nn%2==1){
+        //         ans = ans*x;
+        //         nn = nn-1;
+        //     }else{
+        //         x = x*x;
+        //         nn=nn/2;
+        //     }
+        // }
+        // if(n<0) ans = (double)(1.0)/(double)(ans);
+        // return ans;
+        long long num = n;
+        if(num<0){
+            x=1.0/x;
+            num=-num;
         }
-        if(n<0) ans = (double)(1.0)/(double)(ans);
-        return ans;
+        return power(x,num);
+
 
         
     }
